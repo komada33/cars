3283ddd28906070b03ced16e0b92d22f1eabd3cf
komada33 <alma-alice@docomo.ne.jp>
2022-08-17 09:54:32 +0000

    ユーザー検索追加　css修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
+  before_action :set_search_user
 
   def set_search
     @search = CarPost.ransack(params[:q])
     @search_carposts = @search.result.page(params[:page]).per(5).order(created_at: :desc)
+  end
+
+  def set_search_user
+    @user_search = User.ransack(params[:q])
+    @search_users = @user_search.result.page(params[:page]).per(20).order(created_at: :desc)
   end
 
   protected
